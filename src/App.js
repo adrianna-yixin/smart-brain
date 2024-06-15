3e0e8add52d6fea0c02b6bcbe66a1497f8b3c566
adrianna-yixin <esthergong499@gmail.com>
2024-06-15 20:15:21 +0100

    Add particles background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import ParticlesComponent from './components/Particles/Particles';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <div className="particles" >
+        <ParticlesComponent />
+      </div>
+      <div className='content'>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+      </div>
     </div>
   );
 }
